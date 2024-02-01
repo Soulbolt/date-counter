f58bc4afb9cc21d0b469eaa814500cf98730d2c8
Soulbolt <soulbolt@gmail.com>
2024-01-31 19:50:15 -0700

    FIrst commit, Base files . Include counter function that increments and decreases by 1 when clicking on + or - buttons. DIsplay said buttons and counter respectively.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="message">
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+  return (
+    <>
+      <div>
+        <div>
+          <button className="button" onClick={decreaseCount}>
+            -
+          </button>
+          <span>Count: {count}</span>
+
+          <button className="button" onClick={increaseCount}>
+            +
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
